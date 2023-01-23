18f389aa5a1fb1f63a62dbb2e474ea19db115170
Emmanuel Fofeyin <emmanuel.fofeyin-ngwamunwi@mths.ca>
2023-01-23 16:49:40 +0000

    done 3

---
+++ lesson-03/main.c
@@ -0,0 +1,32 @@
+// copyright (c) 2023 Emmanuel F.N All rights reserved
+//
+// Created by: Emmanuel Fofeyin
+// Created on; Jan 2023
+//This is the Gameboy version of (SA)
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+#include "MetaSprites.c"
+#include "SpaceAliens.c"
+#include "Background.c"
+
+void main () {
+    set_sprite_data(0, 16, SpaceAliens);
+    set_meta_sprite_tile(0, 1, 2, 3, 4);
+    move_meta_sprite(0, 76, 144);
+
+    set_bkg_data(0, 16, SpaceAliens);
+    set_bkg_tiles(0, 0, 20, 36, Background);
+
+    SHOW_BKG;
+    SHOW_SPRITES;
+    DISPLAY_ON;
+
+    // game loop
+    while (1) {
+        scroll_bkg(0, -1);
+
+        wait_vbl_done();
+    }
+}
