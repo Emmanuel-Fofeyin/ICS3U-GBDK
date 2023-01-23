81046130f63585c5fd3ad1fcc7549c0383af14ae
Emmanuel Fofeyin <emmanuel.fofeyin-ngwamunwi@mths.ca>
2023-01-23 00:45:23 +0000

    all new files

---
+++ lesson-01/main.c
@@ -0,0 +1,12 @@
+// copyright (c) 2023 Emmanuel F.N All rights reserved
+//
+// Created by: Emmanuel Fofeyin
+// Created on; Jan 2023
+//This is the Gameboy version of (SA)
+
+#include <gb/gb.h>
+#include <stdio.h>
+
+void main() {
+    printf("Hello, World!");
+}
